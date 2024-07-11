930af7fea2af6cc6b5c7a351956457a49f4cc74b
Sampritikp <saisampritikp10@gmail.com>
2024-07-11 12:06:40 +0530

    2 commit

--- martz/src/components/Products.js
+++ martz/src/components/Products.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React,{components} from 'react'
 import './mystyle.css'
 
- function Products() {
-  return (
-    <div>
-      <h1 className = 'heading'>AMZ</h1>
-    </div>
-  )
+  class Products extends components {
+    constructor()
+    {
+        super()
+        this.state = {
+            Products : 'Earbuds1'
+                   
+        }
+    }
+    details()
+    {
+        this.setState({
+            details:'productName : Earbuds1'
+        })
+    }
+    render(){
+        return (
+            <div>
+              <h1 className = 'heading'>AMZ- Earphone</h1>
+              <p>
+                    <h3> Products</h3>
+                    <button onClick ={()=>"this.details()"}>Earbuds1</button>
+                    <button>Earbuds2</button>
+                    <button>Earbuds3</button>
+                    <h3> {this.state.details}</h3>
+              </p>
+            </div> 
+          )
+    }
+  
 }
 export default Products 
