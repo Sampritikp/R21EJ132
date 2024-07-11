75fbb719cbd6bc2bae0b1479b373592f46ebadda
Sampritikp <saisampritikp10@gmail.com>
2024-07-11 11:33:11 +0530

    initial commit

---
+++ martz/src/components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './mystyle.css'
+
+ function Products() {
+  return (
+    <div>
+      <h1 className = 'heading'>AMZ</h1>
+    </div>
+  )
+}
+export default Products 
